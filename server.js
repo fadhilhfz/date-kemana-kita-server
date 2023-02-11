853a8be5d21042e0c5ea6c765d95787676c77af8
Fadhil H <fadhilhfz369@gmail.com>
2023-02-11 18:33:08 +0700

    Misc

--- server.js
+++ server.js
@@ -1,27 +1,11 @@
-const db = require("./models/index");
 const express = require("express");
+const db = require("./models/index");
+const ideasRouter = require("./routes/ideasRoute");
 const app = express();
 const PORT = 3000;
 
-db.sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Connection has been established successfully.");
-    // Perform database operations here
-  })
-  .catch((err) => {
-    console.error("Unable to connect to the database:", err);
-  });
+app.use("/ideas", ideasRouter);
 
-app.get("/", async (req, res) => {
-  const randomIdea = await db.idea.findOne({
-    order: [db.sequelize.literal("RANDOM()")],
-  });
-  // const plainIdeas = ideas.map((i) => i.toJSON());
-  console.log(randomIdea);
-  res.json(randomIdea);
-});
-
-app.listen(PORT, (req, res) => {
+app.listen(PORT, () => {
   console.log(`Server start at port ${PORT}`);
 });
