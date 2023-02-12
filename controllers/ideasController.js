049e4c3bb02165d076c677fc14a59958394ace2f
Fadhil H <fadhilhfz369@gmail.com>
2023-02-12 15:29:25 +0700

    Change getRandomIdea() to also include another model
    getRandomIdea() from ideasController now include other model too, like
    fee, image_url, subcategory, ideas_subcategory

--- controllers/ideasController.js
+++ controllers/ideasController.js
@@ -2,6 +2,33 @@ const db = require("../models/index");
 
 async function getRandomIdea() {
   const randomIdea = await db.idea.findOne({
+    include: [
+      {
+        model: db.fee,
+        attributes: ["weekdays_fee", "weekend_fee", "single_fee"],
+      },
+      {
+        model: db.image_url,
+        attributes: ["url"],
+      },
+      {
+        model: db.subcategory,
+        attributes: ["name"],
+        through: {
+          attributes: [],
+        },
+      },
+    ],
+    attributes: [
+      "name",
+      "category",
+      "description",
+      "latitude",
+      "longitude",
+      "ofc_web",
+      "ofc_twt",
+      "ofc_ig",
+    ],
     order: [db.sequelize.literal("RANDOM()")],
   });
   return randomIdea;
--- routes/ideasRoute.js
+++ routes/ideasRoute.js
@@ -15,4 +15,11 @@ ideasRouter.get("/:id", async (req, res, next) => {
   res.send(result);
 });
 
+// ideasRouter.get("/bogor", async (req, res, next) => {
+//   const result = await ideasController.getKRB();
+//   // if (result.error) return res.status(404).send(result);
+
+//   res.send(result);
+// });
+
 module.exports = ideasRouter;
