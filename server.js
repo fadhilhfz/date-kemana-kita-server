ed6b579402a8f85ff8f17389a8599f9d2208aad7
Fadhil H <fadhilhfz369@gmail.com>
2023-02-12 15:31:02 +0700

    Misc

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
-const db = require("./models/index");
-const ideasRouter = require("./routes/ideasRoute");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const ideasRouter = require("./routes/ideasRoute");
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.use("/ideas", ideasRouter);
 
